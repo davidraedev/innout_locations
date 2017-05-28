27db531a0ae056da9ec528284d583f29311f3377
daraeman <daraeman@gmail.com>
2017-05-28 00:24:21 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var fetch = require( "node-fetch" );
+
+var url = "http://locations.in-n-out.com/api/finder/search";
+
+var locations = null;
+var fetch_time = null;
+
+var data = function() {
+	return {
+		retrieved_at: fetch_time,
+		data: locations,
+	};
+};
+
+var get = new Promise( function( resolve, reject ) {
+	var self = this;
+
+	fetch_time = new Date();
+
+	fetch( url )
+		.then( function( res ) {
+			return res.json();
+		})
+		.then( function( json ) {
+			locations = json;
+			resolve( data() );
+		})
+		.catch( function( error ) {
+			reject( error );
+		});
+
+});
+
+module.exports = {
+	get: get,
+	data: data,
+};
---
+++ test/get.js
@@ -0,0 +1,46 @@
+var chai = require( "chai" );
+chai.use( require( "chai-json-schema" ) );
+var expect = chai.expect;
+
+var innoutLocations = require( "../index.js" );
+
+var storeSchema = {
+	retrieved_at: { type: "date" },
+	data: [
+		{
+			StoreNumber: { type: "number" },
+			Name: { type: "string" },
+			StreetAddress: { type: "string" },
+			City: { type: "string" },
+			State: { type: "string" },
+			ZipCode: { type: "number" },
+			Latitude: { type: "number" },
+			Longitude: { type: "number" },
+		}
+	]
+};
+
+describe( "Request Data", function() {
+
+	it( "Retrieves valid JSON", function() {
+
+		return innoutLocations.get.then( function( json ){
+			expect( json ).to.be.jsonSchema( storeSchema );
+		}).catch( function( error ){
+			throw new Error( error );
+		});
+
+	});
+
+});
+
+describe( "Get Saved Data", function() {
+
+	it( "Returns valid JSON", function() {
+
+		var json = innoutLocations.data();
+		expect( json ).to.be.jsonSchema( storeSchema );
+
+	});
+
+});
